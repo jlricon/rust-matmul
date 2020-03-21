6fdf9d492117601959bce1fb197d0ff797f3fd70
Jose Luis <joseluisr@twitter.com>
2020-03-21 14:12:24 +0000

    Static

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Matrix multplication in Rust
-Compile and run with `RUSTFLAGS="-C target-cpu=native -C lto" cargo bench `. Tested on a Mac
+Compile and run with `RUSTFLAGS="-C target-cpu=native -C codegen-units=1" cargo bench`. Tested on a Mac
--- benches/base_case.rs
+++ benches/base_case.rs
@@ -3,9 +3,15 @@ use criterion::{
 };
 type Matrix = Vec<Vec<f32>>;
 use matrixmultiply::sgemm;
-use nalgebra::{Dynamic, Matrix as NMatrix, VecStorage};
-use ndarray::{Array, ArrayBase};
+use nalgebra::{ArrayStorage, Dynamic, Matrix as NMatrix, VecStorage};
+use nalgebra::{U100, U20};
+use ndarray::Array;
 type DMatrix = NMatrix<f32, Dynamic, Dynamic, VecStorage<f32, Dynamic, Dynamic>>;
+type SMatrix1000 = NMatrix<f32, U1000, U1000, ArrayStorage<f32, U1000, U1000>>;
+type SMatrix100 = NMatrix<f32, U100, U100, ArrayStorage<f32, U100, U100>>;
+type SMatrix200 = NMatrix<f32, U200, U200, ArrayStorage<f32, U200, U200>>;
+type SMatrix20 = NMatrix<f32, U20, U20, ArrayStorage<f32, U20, U20>>;
+use typenum::{U1000, U200};
 fn get_matrices(n: usize) -> (Matrix, Matrix) {
     let a: Matrix = vec![vec![0.1; n]; n];
     let b: Matrix = vec![vec![0.1; n]; n];
@@ -16,6 +22,43 @@ fn matmul_nalgebra(n: usize) {
     let b = DMatrix::from_element(n, n, 0.1);
     let o = a.dot(&b);
 }
+fn matmul_nalgebra_static(n: usize) {
+    match n {
+        1000 => {
+            let a = SMatrix1000::repeat(0.1);
+            let b = SMatrix1000::repeat(0.1);
+            let o = a.dot(&b);
+        }
+        100 => {
+            let a = SMatrix100::repeat(0.1);
+            let b = SMatrix100::repeat(0.1);
+            let o = a.dot(&b);
+        }
+        20 => {
+            let a = SMatrix20::repeat(0.1);
+            let b = SMatrix20::repeat(0.1);
+            let o = a.dot(&b);
+        }
+        200 => {
+            let a = SMatrix200::repeat(0.1);
+            let b = SMatrix200::repeat(0.1);
+            let o = a.dot(&b);
+        }
+        _ => unimplemented!(),
+    }
+}
+fn matmul_baseline_static(n: usize) {
+    match n {
+        1000 => {
+            let a = [[0.1 as f32; 1000]; 1000];
+            let b = [[0.1 as f32; 1000]; 1000];
+            let o = matmul_static(&a, &b);
+        }
+
+        _ => unimplemented!(),
+    }
+}
+
 fn matmul_matrixmultiply(n: usize) {
     let a = vec![0.1; n * n];
     let b = vec![0.1; n * n];
@@ -44,6 +87,20 @@ fn matmul(n: usize) {
             }
         }
     }
+}
+fn matmul_static(a: &[[f32; 1000]; 1000], b: &[[f32; 1000]; 1000]) -> [[f32; 1000]; 1000] {
+    let mut out = [[0.0 as f32; 1000]; 1000];
+    for i in 0..1000 {
+        for j in 0..1000 {
+            for k in 0..1000 {
+                unsafe {
+                    *out.get_unchecked_mut(i).get_unchecked_mut(j) +=
+                        a.get_unchecked(i).get_unchecked(k) * b.get_unchecked(k).get_unchecked(j);
+                }
+            }
+        }
+    }
+    out
 }
 fn matmul_opt1(n: usize) {
     let (a, b) = get_matrices(n);
@@ -79,12 +136,21 @@ fn base_matmul(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::new("Nalgebra", n), n, |b, &n| {
             b.iter(|| matmul_nalgebra(n));
         });
+        group.bench_with_input(BenchmarkId::new("NalgebraStatic", n), n, |b, &n| {
+            b.iter(|| matmul_nalgebra_static(n));
+        });
+        if *n == 1000 {
+            group.bench_with_input(BenchmarkId::new("UncheckedStatic", n), n, |b, &n| {
+                b.iter(|| matmul_baseline_static(1000));
+            });
+        }
     }
+
     group.finish();
 }
 
 fn alternate_measurement() -> Criterion {
-    Criterion::default().sample_size(10)
+    Criterion::default().sample_size(20)
 }
 criterion_group!(name=benches;
                 config = alternate_measurement();
